ce9a50234b1f3ad0f15666baea248f17b94a56fe
Nazam <nazamzahoor@yahoo.com>
2018-09-06 23:56:27 -0700

    update burger controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,4 +14,29 @@ router.get("/", function(req,res){
     });
 });
 
+router.post("/create", function(req, res){
+    burger.create(["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function(result){
+        res.json({id: result.insertId});
+    }
+)
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result){
+        if (result.changedRows == 0){
+            return res.status(404).end();
+        }else{
+            res.status(200).end();
+        }
+    }
+    )
+});
+
 module.exports = router;
